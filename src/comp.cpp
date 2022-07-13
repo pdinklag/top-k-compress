617b99e00c17a8428ff043f0f631fcaacad318cb
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-07-13 08:48:44 +0200

    Decompression outline; only top-k maintenance missing

--- src/comp.cpp
+++ src/comp.cpp
@@ -2,21 +2,24 @@
 #include <tdc/io/file_input_stream.hpp>
 #include <tdc/io/file_output_stream.hpp>
 #include <tdc/io/stream_input_iterator.hpp>
+#include <tdc/io/stream_output_iterator.hpp>
 #include <tdc/io/util.hpp>
 #include "top_k_compress.hpp"
 
 using namespace tdc::framework;
 
 struct Options : public Entity {
+    bool decompress = false;
     uint64_t k = 8;
     uint64_t window = 4;
     uint64_t sketch_rows = 2;
     uint64_t sketch_columns = 8;
 
     Options() : Entity("top-k-compress", "Compression using top-k substrings") {
+        param('d', "decompress", decompress, "Decompress the input file; all other parameters are ignored.");
         param('k', "num-frequent", k, "The number of frequent substrings to maintain.");
         param('w', "window", window, "The window size.");
-        param('d', "sketch-rows", sketch_rows, "The number of rows in the Count-Min sketch.");
+        param('r', "sketch-rows", sketch_rows, "The number of rows in the Count-Min sketch.");
         param('c', "sketch-columns", sketch_columns, "The number of columns in each Count-Min row.");
     }
 };
@@ -27,9 +30,15 @@ int main(int argc, char** argv) {
     Application app(options, argc, argv);
     if(app) {
         if(!app.args().empty()) {
-            tdc::io::FileInputStream fis(app.args()[0]);
-            tdc::io::FileOutputStream fos(app.args()[0] + ".topk");
-            top_k_compress(fis.begin(), fis.end(), tdc::io::bitwise_output_to(fos), options.k, options.window, options.sketch_rows, options.sketch_columns);
+            if(options.decompress) {
+                tdc::io::FileInputStream fis(app.args()[0]);
+                tdc::io::FileOutputStream fos(app.args()[0] + ".dec");
+                top_k_decompress(tdc::io::bitwise_input_from(fis), tdc::io::StreamOutputIterator(fos));
+            } else {
+                tdc::io::FileInputStream fis(app.args()[0]);
+                tdc::io::FileOutputStream fos(app.args()[0] + ".topk");
+                top_k_compress(fis.begin(), fis.end(), tdc::io::bitwise_output_to(fos), options.k, options.window, options.sketch_rows, options.sketch_columns);
+            }
         } else {
             app.print_usage(options);
         }
--- src/top_k_compress.hpp
+++ src/top_k_compress.hpp
@@ -1,4 +1,6 @@
 #include <bit>
+#include <cstdint>
+#include <cstdlib>
 #include <iostream>
 
 #include <tlx/container/ring_buffer.hpp>
@@ -9,13 +11,28 @@
 
 #include "top_k_substrings.hpp"
 
+constexpr uint64_t MAGIC = 0x54'4F'50'4B'43'4F'4D'50ULL; // spells "TOPKCOMP" in hex
+
 template<tdc::InputIterator<char> In, tdc::io::BitSink Out>
 void top_k_compress(In begin, In const end, Out out, size_t const k, size_t const window_size, size_t const sketch_rows, size_t const sketch_columns) {
-    tlx::RingBuffer<char> buf(window_size);
-    TopKSubstrings topk(k, window_size, sketch_rows, sketch_columns);
+    using namespace tdc::code;
 
-    tdc::code::Universe const u_literal(0, 255);
-    tdc::code::Universe const u_freq(0, k-1);
+    // encode header
+    Binary::encode(out, MAGIC, Universe::of<uint64_t>());
+    Binary::encode(out, k, Universe::of<size_t>());
+    Binary::encode(out, window_size, Universe::of<size_t>());
+    Binary::encode(out, sketch_rows, Universe::of<size_t>());
+    Binary::encode(out, sketch_columns, Universe::of<size_t>());
+
+    // initialize compression
+    // notes on top-k:
+    // - frequent substring 0 is reserved to indicate a literal character
+    // - frequent substring k-1 is reserved to indicate the end of file
+    tlx::RingBuffer<char> buf(window_size);
+    TopKSubstrings topk(k-1, window_size, sketch_rows, sketch_columns);
+
+    Universe const u_literal(0, 255);
+    Universe const u_freq(0, k-1);
 
     size_t num_frequent = 0;
     size_t num_literal = 0;
@@ -35,14 +52,14 @@ void top_k_compress(In begin, In const end, Out out, size_t const k, size_t cons
             // encode phrase
             if(i >= next_phrase) {
                 if(longest.length > 1) {
-                    tdc::code::Binary::encode(out, longest.index, u_freq);
+                    Binary::encode(out, longest.index, u_freq);
 
                     ++num_frequent;
                     next_phrase = i + longest.length;
                     naive_enc_size += u_freq.entropy();
                 } else {
-                    tdc::code::Binary::encode(out, 0, u_freq);
-                    tdc::code::Binary::encode(out, c, u_literal);
+                    Binary::encode(out, 0, u_freq);
+                    Binary::encode(out, c, u_literal);
 
                     ++num_literal;
                     next_phrase = i + 1;
@@ -65,6 +82,9 @@ void top_k_compress(In begin, In const end, Out out, size_t const k, size_t cons
         handle(0, window_size - 1 - x);
     }
 
+    // encode EOF
+    Binary::encode(out, k-1, u_freq);
+
     // debug
     topk.print_debug_info();
     std::cout << "parse"
@@ -77,27 +97,65 @@ void top_k_compress(In begin, In const end, Out out, size_t const k, size_t cons
 }
 
 template<tdc::io::BitSource In, std::output_iterator<char> Out>
-void top_k_decompress(In in, Out out, size_t const k, size_t const window_size, size_t const sketch_rows, size_t const sketch_columns) {
-    tlx::RingBuffer<char> buf(window_size);
-    TopKSubstrings topk(k, window_size, sketch_rows, sketch_columns);
+void top_k_decompress(In in, Out out) {
+    using namespace tdc::code;
 
-    tdc::code::Universe const u_literal(0, 255);
-    tdc::code::Universe const u_freq(0, k-1);
+    // decode header
+    uint64_t const magic = Binary::decode(in, Universe::of<uint64_t>());
+    if(magic != MAGIC) {
+        std::cerr << "wrong magic: 0x" << magic << std::endl;
+        return;
+    }
+
+    size_t const k = Binary::decode(in, Universe::of<size_t>());
+    size_t const window_size = Binary::decode(in, Universe::of<size_t>());
+    size_t const sketch_rows = Binary::decode(in, Universe::of<size_t>());
+    size_t const sketch_columns = Binary::decode(in, Universe::of<size_t>());
+
+    // initialize
+    tlx::RingBuffer<char> buf(window_size);
+    TopKSubstrings topk(k-1, window_size, sketch_rows, sketch_columns);
+
+    Universe const u_literal(0, 255);
+    Universe const u_freq(0, k-1);
+
+    size_t num_frequent = 0;
+    size_t num_literal = 0;
+
     char frequent_string[window_size];
 
-    while(true) { // TODO: when to stop?
-        auto const p = tdc::code::Binary::decode(in, u_freq);
+    while(true) {
+        auto const p = Binary::decode(in, u_freq);
+        if(p == k-1) {
+            // EOF
+            break;
+        }
         if(p) {
             // decode frequent phrase
-            auto const len = topk.get(p, frequent_string);
-            for(size_t i = 0; i < len; i++) {
-                *out++ = frequent_string[i];
+            ++num_frequent;
+
+            // auto const len = topk.get(p, frequent_string);
+            // for(size_t i = 0; i < len; i++) {
+                // *out++ = frequent_string[i];
                 // TODO: update top-k
-            }
+            // }
         } else {
             // decode literal phrase
-            *out++ = (char)tdc::code::Binary::decode(in, u_literal);
+            ++num_literal;
+            char const c = Binary::decode(in, u_literal);
+            // *out++ = c;
             // TODO: update top-k
         }
     }
+
+    // debug
+    std::cout << "decompress"
+        << " k=" << k
+        << " w=" << window_size
+        << " c=" << sketch_columns
+        << " r=" << sketch_rows
+        << ": num_frequent=" << num_frequent
+        << ", num_literal=" << num_literal
+        << " -> total phrases: " << (num_frequent + num_literal)
+        << std::endl;
 }
