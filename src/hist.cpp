3caf0041cd210d6455054bce0d17298d730360d7
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-08-02 11:09:53 +0200

    hist tool

---
+++ src/hist.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <tdc/io/file_input_stream.hpp>
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        std::cerr << "usage: " << argv[0] << " [FILE]" << std::endl;
+        return -1;
+    }
+
+    size_t hist[256] = { 0 };
+
+    tdc::io::FileInputStream fis(argv[1]);
+    for(char const c : fis) {
+        ++hist[(unsigned char)c];
+    }
+
+    for(size_t i = 0; i < 256; i++) {
+        std::cout << i << "\t" << hist[i];
+        if(i >= 32 && i < 127) std::cout << "\t# '" << (char)i << "'";
+        std::cout << std::endl;
+    }
+}
