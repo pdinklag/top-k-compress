bf7a0291b44e08871d0a65d24e2790803a65be1b
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-08-05 08:29:32 +0200

    Switch from FCNS to label-string representation of trie

--- src/trie_filter.hpp
+++ src/trie_filter.hpp
@@ -9,19 +9,44 @@ template<std::unsigned_integral Frequency>
 class TrieFilter {
 private:
     struct Node {
-        size_t first_child;
-        size_t next_sibling;
-        char label;
+        size_t size;
+        size_t capacity;
+        std::unique_ptr<char[]>   labels;
+        std::unique_ptr<size_t[]> children;
+        
         Frequency freq;
         Frequency insert_freq;
         size_t parent;
         uint64_t fingerprint;
+        char label;
+        
+        Node(char const _label, Frequency const _freq, size_t const _parent, uint64_t const _fingerprint)
+            : size(0),
+              capacity(0),
+              freq(_freq),
+              insert_freq(_freq),
+              parent(_parent),
+              fingerprint(_fingerprint),
+              label(_label) {
+        }
+        
+        Node() : Node(0, 0, 0, 0) {
+        }
     };
 
     size_t capacity_;
     size_t size_;
 
     std::unique_ptr<Node[]> nodes_;
+
+    bool is_child_of(size_t const node, size_t const parent) const {
+        for(size_t i = 0; i < nodes_[parent].size; i++) {
+            if(nodes_[parent].children[i] == node) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 public:
     struct ExtractInfo {
@@ -33,7 +58,7 @@ public:
     TrieFilter(size_t const capacity) : capacity_(capacity), size_(1) {
         nodes_ = std::make_unique<Node[]>(capacity_);
         for(size_t i = 0; i < capacity_; i++) {
-            nodes_[i] = Node { 0, 0, (char)0, 0, 0, 0, 0 };
+            nodes_[i] = Node();
         }
     }
 
@@ -41,9 +66,30 @@ public:
         size_t discard;
         assert(!try_get_child<false>(parent, label, discard));
 
-        auto sibling = nodes_[parent].first_child;
-        nodes_[parent].first_child = node;
-        nodes_[node] = Node { 0, sibling, label, freq, freq, parent, fingerprint };
+        //auto sibling = nodes_[parent].first_child;
+        //nodes_[parent].first_child = node;
+        
+        // possibly allocate new child slots
+        auto& p = nodes_[parent];
+        if(p.size >= p.capacity) {
+            size_t const new_cap = std::max(size_t(1), 2 * p.capacity);
+            auto new_labels = std::make_unique<char[]>(new_cap);
+            auto new_children = std::make_unique<size_t[]>(new_cap);
+            
+            std::copy(p.labels.get(), p.labels.get() + p.size, new_labels.get());
+            std::copy(p.children.get(), p.children.get() + p.size, new_children.get());
+            
+            p.capacity = new_cap;
+            p.labels = std::move(new_labels);
+            p.children = std::move(new_children);
+        }
+        
+        // append child
+        p.labels[p.size] = label;
+        p.children[p.size] = node;
+        ++p.size;
+        
+        nodes_[node] = Node(label, freq, parent, fingerprint);
 
         assert(is_child_of(node, parent));
         assert(is_leaf(node));
@@ -54,27 +100,27 @@ public:
 
         auto const parent = nodes_[node].parent;
         assert(is_child_of(node, parent));
-
-        auto fc = nodes_[parent].first_child;
-        if(fc == node) {
-            // removing the first child
-            nodes_[parent].first_child = nodes_[node].next_sibling;
+        
+        auto const label = nodes_[node].label;
+        
+        auto& p = nodes_[parent];
+        size_t i = 0;
+        while(i < p.size && p.labels[i] != label) ++i; // nb: we could also search in the child array, but searching in the labels causes fewer cache misses
+        
+        if(i < p.size) {
+            assert(p.children[i] == node);
+            
+            // swap with last child if necessary and reduce size
+            if(p.size > 1) {
+                size_t const last = p.size - 1;
+                std::swap(p.labels[i], p.labels[last]);
+                std::swap(p.children[i], p.children[last]);
+            }
+            --p.size;
+            // TODO: possibly re-allocate?
         } else {
-            // removing some other child
-            // find previous sibling and remove
-            auto prev_sibling = fc;
-            auto v = nodes_[fc].next_sibling;
-            while(v && v != node) {
-                prev_sibling = v;
-                v = nodes_[v].next_sibling;
-            }
-
-            if(v == node) {
-                nodes_[prev_sibling].next_sibling = nodes_[node].next_sibling;
-            } else {
-                // "this kid is not my son"
-                assert(false);
-            }
+            // "this kid is not my son"
+            assert(false);
         }
 
         assert(nodes_[node].freq >= nodes_[node].insert_freq);
@@ -91,31 +137,14 @@ public:
 
     template<bool mtf = true>
     bool try_get_child(size_t const node, char const label, size_t& out_child) {
-        auto const fc = nodes_[node].first_child;
-        auto v = fc;
-        size_t ps = 0;
-        while(v) {
-            auto const ns = nodes_[v].next_sibling;
-            if(nodes_[v].label == label) {
-                out_child = v;
-
-                if(mtf && v != fc) {
-                    assert(ps);
-
-                    // MTF
-                    nodes_[node].first_child = v;
-                    nodes_[v].next_sibling = fc;
-                    nodes_[ps].next_sibling = ns;
-
-                    assert(is_child_of(v, node));
-                    assert(is_child_of(fc, node));
-                    assert(is_child_of(ps, node));
-                    if(ns) assert(is_child_of(ns, node));  
+        for(size_t i = 0; i < nodes_[node].size; i++) {
+            if(nodes_[node].labels[i] == label) {
+                out_child = nodes_[node].children[i];
+                if constexpr(mtf) {
+                    // no longer implemented since switching to label-string-representation
                 }
                 return true;
             }
-            ps = v;
-            v = ns;
         }
         return false;
     }
@@ -125,18 +154,7 @@ public:
     }
 
     bool is_leaf(size_t const node) const {
-        return nodes_[node].first_child == 0;
-    }
-
-    bool is_child_of(size_t const node, size_t const parent) const {
-        auto fc = nodes_[parent].first_child;
-        if(fc == node) return true;
-
-        auto v = nodes_[fc].next_sibling;
-        while(v && v != node) {
-            v = nodes_[v].next_sibling;
-        }
-        return v == node;
+        return nodes_[node].size == 0;
     }
 
     size_t root() const {
