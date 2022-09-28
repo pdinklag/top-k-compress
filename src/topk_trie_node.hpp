7fdda89a674b953d88922f9feb0f75cb537608c9
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-09-28 08:47:52 +0200

    TopkTrieNodeData mixin

--- src/topk_trie_node.hpp
+++ src/topk_trie_node.hpp
@@ -3,16 +3,16 @@
 #include "min_pq.hpp"
 #include "trie_node.hpp"
 
-template<std::unsigned_integral NodeIndex = uint32_t>
-struct TopkTrieNode : public TrieNode<NodeIndex> {
-    using TrieNode<NodeIndex>::Character;
-    using TrieNode<NodeIndex>::Index;
-
+struct TopkTrieNodeData {
     size_t freq;
     size_t insert_freq;
     uint64_t fingerprint;
     MinPQ<size_t>::Location minpq;
+} __attribute__((packed));
 
+template<std::unsigned_integral NodeIndex = uint32_t>
+struct TopkTrieNode : public TrieNode<NodeIndex>, public TopkTrieNodeData {
+    using TrieNode<NodeIndex>::Character;
+    using TrieNode<NodeIndex>::Index;
     using TrieNode<NodeIndex>::TrieNode;
-
 } __attribute__((packed));
