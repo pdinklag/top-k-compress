893c5ce633898fd1a0abe10c978ac221af5e1a27
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-11-24 11:20:57 +0100

    LIDO3 config

--- benchmark/config.py
+++ benchmark/config.py
@@ -1,19 +1,16 @@
-gzip_bin = "/usr/bin/gzip"
-bzip2_bin = "/usr/bin/bzip2"
-topk_bin = "/scratch/top-k-compress/build/src/comp"
-
 ks      = ["1M", "2M", "4M", "8M", "16M", "32M", "48M"]
-windows = [8, 12, 16, 24, 32, 48, 64]
+windows = [8, 12, 16, 24, 32, 48, 64, 96, 128]
 
 files = [
-    "/scratch/data/cere.200Mi",
-    "/scratch/data/english.200Mi",
-    "/scratch/data/dblp.xml.200Mi",
-    "/scratch/data/dna.200Mi",
-    "/scratch/data/einstein.en.txt.200Mi",
-    "/scratch/data/proteins.200Mi",
-    "/scratch/data/random64K.200Mi",
-    "/scratch/data/sources.200Mi",
+    "/work/smpadink/data/random64K.200Mi",
+    "/work/smpadink/data/pc/dblp.xml.200Mi",
+    "/work/smpadink/data/pc/dna.200Mi",
+    "/work/smpadink/data/pc/english.200Mi",
+    "/work/smpadink/data/pc/proteins.200Mi",
+    "/work/smpadink/data/pc/sources.200Mi",
+    "/work/smpadink/data/pc/real/cere.200Mi",
+    "/work/smpadink/data/pc/real/einstein.en.txt.200Mi",
+    "/work/smpadink/data/pc/real/kernel.200Mi",
 ]
 
-workdir = '/tmp'
+workdir = '/scratch'
