ffa87cbccc284bfa3d04e0224ff922e7f704cb99
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-12-08 14:28:31 +0100

    test frame

---
+++ test/test_binary_rank.cpp
@@ -0,0 +1,9 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include <index/binary_rank.hpp>
+
+TEST_SUITE("binary_rank") {
+    TEST_CASE("test1") {
+    }
+}
---
+++ test/test_wt.cpp
@@ -0,0 +1,9 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include <index/wavelet_tree.hpp>
+
+TEST_SUITE("wavelet_tree") {
+    TEST_CASE("test1") {
+    }
+}
