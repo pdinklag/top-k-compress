95bdb4f3ae54f54537f75ddca7997af46ac6cff4
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-12-14 09:53:29 +0100

    Fix backward search

--- include/index/backward_search.hpp
+++ include/index/backward_search.hpp
@@ -25,20 +25,14 @@ public:
     inline BackwardSearch(It begin, It const end) : wt_(begin, end) {
         c_array_[0] = 0;
         for(size_t c = 1; c < sigma_max_; c++) {
-            c_array_[c] = c_array_[c-1] + wt_.occ(Char(c));
+            c_array_[c] = c_array_[c-1] + wt_.occ(Char(c-1));
         }
-    }
-
-    inline Interval init(Char const c) const {
-        auto const x = UChar(c);
-        return { c_array_[c], c < sigma_max_ ? c_array_[c+1] : wt_.length() };
     }
 
     inline Interval step(Interval const x, Char const c) const {
         auto const off = c_array_[c];
-        return {
-            off + (x.first  > 0 ? wt_.rank(c, x.first-1 ) : 0),
-            off + (x.second > 0 ? wt_.rank(c, x.second-1) : 0)
-        };
+        auto const s = off + (x.first > 0 ? wt_.rank(c, x.first-1) : 0);
+        auto const e = off + wt_.rank(c, x.second) - 1;
+        return { s, e };
     }
 };
