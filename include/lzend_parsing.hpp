4193b9858c1279035eea5344047574ff42d710e5
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2023-02-16 10:06:41 +0100

    LZ-End parsing string extraction

---
+++ include/lzend_parsing.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <cassert>
+#include <concepts>
+#include <iterator>
+#include <vector>
+
+#include <display.hpp>
+#include <index/btree.hpp>
+
+template<std::integral Char, std::unsigned_integral Index>
+class LZEndParsing {
+public:
+    struct Phrase {
+        Index link;
+        Index len;
+        Index end;
+        Char last;
+    } __attribute__((packed));
+
+private:
+    struct PhraseEnd {
+        Index pos;
+        Index phr;
+
+        // std::totally_ordered
+        bool operator==(PhraseEnd const& x) const { return pos == x.pos; }
+        bool operator!=(PhraseEnd const& x) const { return pos != x.pos; }
+        bool operator< (PhraseEnd const& x) const { return pos <  x.pos; }
+        bool operator<=(PhraseEnd const& x) const { return pos <= x.pos; }
+        bool operator> (PhraseEnd const& x) const { return pos >  x.pos; }
+        bool operator>=(PhraseEnd const& x) const { return pos >= x.pos; }
+    } __attribute__((packed));
+
+    Index len_;
+    std::vector<Phrase> phrases_;
+    BTree<PhraseEnd, 65> ends_;
+
+public:
+    LZEndParsing() : len_(0) {
+        phrases_.emplace_back(0, 0, -1, 0);
+    }
+
+    // inserts a new LZ-End phrase at the end
+    void emplace_back(Index const link, Index const len, Char const last) {
+        assert(len);
+
+        auto const p = (Index)phrases_.size();
+        len_ += len;
+        phrases_.emplace_back(link, len, len_ - 1, last);
+        ends_.insert({len_ - 1, p});
+    }
+
+    // extracts the substring of the text of given length and starting at the given position
+    template<std::output_iterator<Char> Out>
+    void extract(Out out, Index const start, Index const len) const {
+        auto const end = start + len - 1;
+        auto const r = ends_.successor({end,0});
+        assert(r.exists);
+        auto const p = r.key.phr;
+        if(r.key.pos == end) {
+            // we're at a phrase end position
+            if(len > 1) extract(out, start, len - 1);
+            *out++ = phrases_[p].last;
+        } else {
+            // we're somewhere within phrase p and need to extract something from the source
+            auto const pstart = phrases_[p-1].end + 1;
+            auto const lnk_end = phrases_[phrases_[p].link].end;
+
+            if(start < pstart) {
+                // we are trying to extract some part prior to the current phrase, and that must be done separately
+                extract(out, start, pstart - start);
+
+                // extract remainder from source
+                auto const suffix_len = end - pstart + 1;
+                extract(out, lnk_end - suffix_len + 1, suffix_len);
+            } else {
+                // extract from source
+                extract(out, lnk_end - len + 1, len);
+            }
+        }
+    }
+
+    // extracts the i-th phrase
+    template<std::output_iterator<Char> Out>
+    void extract_phrase(Out out, Index const i) const {
+        auto const len = phrases_[i].len;
+        extract(out, phrases_[i].end - len + 1, len);
+    }
+
+    // gets the number of the phrase (1-based) that the given text position (0-based) lies in
+    Index phrase_at(Index const text_pos) const {
+        auto const r = ends_.successor({text_pos,0});
+        return r.exists ? r.key.phr : 0;
+    }
+
+    // gets the length of the represented text
+    Index length() const { return len_; }
+
+    // gets the number of phrases
+    auto size() const { return phrases_.size() - 1; }
+
+    // gets the i-th phrase (1-based)
+    Phrase operator[](Index const i) const { assert(i > 0); return phrases_[i]; }
+};
--- test/test_kk.cpp
+++ test/test_kk.cpp
@@ -2,13 +2,67 @@
 #include "doctest.h"
 
 #include <kk_trie.hpp>
+#include <lzend_parsing.hpp>
 
 TEST_SUITE("kempa_kosolobov_2017") {
+    TEST_CASE("lzend") {
+        LZEndParsing<char, uint32_t> parsing;
+        parsing.emplace_back(0, 1, 'a'); // a
+        parsing.emplace_back(0, 1, 'b'); // b
+        parsing.emplace_back(2, 3, 'b'); // abb
+        parsing.emplace_back(3, 5, 'a'); // babba
+        parsing.emplace_back(4, 8, 'a'); // bbbabbaa
+
+        REQUIRE(parsing.size() == 5);
+        REQUIRE(parsing.length() == 18);
+        REQUIRE(parsing.phrase_at(0) == 1);
+        REQUIRE(parsing.phrase_at(1) == 2);
+        REQUIRE(parsing.phrase_at(2) == 3);
+        REQUIRE(parsing.phrase_at(3) == 3);
+        REQUIRE(parsing.phrase_at(4) == 3);
+        REQUIRE(parsing.phrase_at(5) == 4);
+        REQUIRE(parsing.phrase_at(6) == 4);
+        REQUIRE(parsing.phrase_at(7) == 4);
+        REQUIRE(parsing.phrase_at(8) == 4);
+        REQUIRE(parsing.phrase_at(9) == 4);
+        REQUIRE(parsing.phrase_at(10) == 5);
+        REQUIRE(parsing.phrase_at(11) == 5);
+        REQUIRE(parsing.phrase_at(12) == 5);
+        REQUIRE(parsing.phrase_at(13) == 5);
+        REQUIRE(parsing.phrase_at(14) == 5);
+        REQUIRE(parsing.phrase_at(15) == 5);
+        REQUIRE(parsing.phrase_at(16) == 5);
+        REQUIRE(parsing.phrase_at(17) == 5);
+
+        std::string s;
+        s.reserve(18);
+        {
+            parsing.extract(std::back_inserter(s), 0, 18);
+            REQUIRE(s == "ababbbabbabbbabbaa");
+        }
+        s.clear();
+        {
+            parsing.extract_phrase(std::back_inserter(s), 3);
+            REQUIRE(s == "abb");
+        }
+        s.clear();
+        {
+            parsing.extract_phrase(std::back_inserter(s), 4);
+            REQUIRE(s == "babba");
+        }
+        s.clear();
+        {
+            parsing.extract_phrase(std::back_inserter(s), 5);
+            REQUIRE(s == "bbbabbaa");
+        }
+    }
+
     TEST_CASE("rst") {
         using Index = uint32_t;
+        static constexpr Index MAX = 256; //4096;
 
-        for(Index y = 0; y <= 4095; y++) {
-            for(Index x = y + 1; x <= 4096; x++) {
+        for(Index y = 0; y < MAX; y++) {
+            for(Index x = y + 1; x <= MAX; x++) {
                 auto const i = max_i_rst(x, y);
                 auto const p = rst(x, i);
 
