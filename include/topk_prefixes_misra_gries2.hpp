3a8c71ad95446369c2d7b18f5339d72bf54d4cf8
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2023-10-19 08:10:03 +0200

    Limit frequencies and renormalize if needed; use plain array over hash table for bucket ends

--- include/topk_prefixes_misra_gries2.hpp
+++ include/topk_prefixes_misra_gries2.hpp
@@ -22,7 +22,7 @@
 template<std::unsigned_integral TrieNodeIndex = uint32_t>
 class TopKPrefixesMisraGries2 {
 private:
-    static constexpr bool gather_stats_ = true;
+    static constexpr bool gather_stats_ = false;
 
     struct NodeData : public TrieNode<TrieNodeIndex> {
         using Character = TrieNode<TrieNodeIndex>::Character;
@@ -42,40 +42,71 @@ private:
         size_t freq;
     } __attribute__((packed));
 
-    static constexpr bool DEBUG = false;
-
     using TrieNodeDepth = TrieNodeIndex;
 
+    static constexpr TrieNodeIndex BUCKET_DOES_NOT_EXIST = -1;
+    static constexpr size_t renorm_divisor_ = 2;
+
     size_t k_;
+    size_t max_allowed_frequency_;
 
     Trie<NodeData, true> trie_;
     std::unique_ptr<NodeFrequency[]> freqs_;                          // the circular Space-Saving array of frequencies
-    ankerl::unordered_dense::map<size_t, TrieNodeIndex> bucket_ends_; // maps frequencies to the slot that marks the end of the corresponding buckets
-    size_t threshold_;                                                // the current "garbage" bucket
-
-    size_t max_freq_; // the highest current frequency
+    std::unique_ptr<TrieNodeIndex[]> bucket_ends_;                    // maps frequencies to the slot that marks the end of the corresponding buckets
+    size_t threshold_;                                                // the current "garbage" frequency
 
     // stats
     struct Stats {
+        size_t max_freq = 0;
         size_t num_increment = 0;
         size_t num_swap = 0;
         size_t num_decrement = 0;
+        size_t num_renormalize = 0;
     } stats_;
 
-    size_t num_slots() const ALWAYS_INLINE {
+    size_t num_freqs() const ALWAYS_INLINE {
         return k_ - 1;
     }
 
     TrieNodeIndex prev_slot(TrieNodeIndex const x) const ALWAYS_INLINE {
         // get the previous slot in a circular manner
-        return (x == 0) ? (num_slots() - 1) : (x - 1);
+        return (x == 0) ? (num_freqs() - 1) : (x - 1);
+    }
+    
+    void renormalize() {
+        if constexpr(gather_stats_) ++stats_.num_renormalize;
+
+        // we normalize the frequency to [0, renorm_divisor_ * max_allowed_frequency_]
+        // for this, we first divide every frequency by renorm_divisor_,
+        // then we substract threshold_ / renorm_divisor_
+        auto const adjusted_threshold = threshold_ / renorm_divisor_;
+        auto renormalize = [&](size_t const f){ return f / renorm_divisor_ - adjusted_threshold; };
+
+        for(size_t i = 0; i < num_freqs(); i++) {
+            auto const f = std::max(freqs_[i].freq, threshold_); // nb: we must NOT allow frequency below the threshold, that would cause negative frequencies
+            freqs_[i].freq = renormalize(f);
+        }
+
+        // compact buckets
+        for(size_t f = 0; f <= max_allowed_frequency_; f++) {
+            if(bucket_ends_[f] != BUCKET_DOES_NOT_EXIST) {
+                // we may update the bucket end for an adjusted frequency multiple times because we do integer division
+                // however, we update in the order of frequencies, so bucket ends will correctly end up to be "rightmost" (in a circular manner)
+                auto const adjusted_f = renormalize(f);
+                bucket_ends_[adjusted_f] = bucket_ends_[f];
+                bucket_ends_[f] = BUCKET_DOES_NOT_EXIST;
+            }
+        }
+
+        // reset threshold
+        threshold_ = 0;
     }
 
     void decrement_all() ALWAYS_INLINE {
         if constexpr(gather_stats_) ++stats_.num_decrement;
 
         // erase current threshold bucket if it exists
-        bucket_ends_.erase(threshold_);
+        bucket_ends_[threshold_] = BUCKET_DOES_NOT_EXIST;
 
         // then simply increment the threshold
         ++threshold_;
@@ -94,10 +125,8 @@ private:
         assert(f + 1 > threshold_);
 
         // swap to end of the corresponding bucket, if necessary
-        auto const it_f = bucket_ends_.find(f);
-        assert(it_f != bucket_ends_.end());
-
-        auto const j = it_f->second;
+        auto const j = bucket_ends_[f];
+        assert(j != BUCKET_DOES_NOT_EXIST);
         if(j != i) {
             if constexpr(gather_stats_) ++stats_.num_swap;
 
@@ -117,28 +146,33 @@ private:
         auto const before_j = prev_slot(j);
         if(freqs_[before_j].freq == f) {
             // the node in the previous slot has the same frequency, move bucket pointer
-            it_f->second = before_j;
+            bucket_ends_[f] = before_j;
         } else {
             // the incremented slot was the last in its bucket, erase the bucket
-            bucket_ends_.erase(it_f);
+            bucket_ends_[f] = BUCKET_DOES_NOT_EXIST;
         }
 
         // increment frequency
         freqs_[j].freq = f + 1;
-        max_freq_ = std::max(f+1, max_freq_);
+        if constexpr(gather_stats_) stats_.max_freq = std::max(f+1, stats_.max_freq);
 
         // create bucket if needed
-        if(!bucket_ends_.contains(f+1)) {
-            bucket_ends_.emplace(f+1, j);
+        if(bucket_ends_[f+1] == BUCKET_DOES_NOT_EXIST) {
+            bucket_ends_[f+1] = j;
+        }
+
+        // possibly renormalize
+        if(f + 1 == max_allowed_frequency_) {
+            if constexpr(gather_stats_) ++stats_.num_renormalize;
+            renormalize();
         }
     }
 
     bool insert(TrieNodeIndex const parent, char const label, TrieNodeIndex& out_node) ALWAYS_INLINE {
-        auto const it = bucket_ends_.find(threshold_);
-        if(it != bucket_ends_.end()) {
+        auto const i = bucket_ends_[threshold_];
+        if(i != BUCKET_DOES_NOT_EXIST) {
             // recycle something from the garbage
-            auto const i = it->second;
-            assert(i < num_slots());
+            assert(i < num_freqs());
 
             // override node in trie
             auto const v = freqs_[i].node;
@@ -167,9 +201,14 @@ public:
     inline TopKPrefixesMisraGries2(size_t const k, size_t const sketch_columns, size_t const fp_window_size = 8)
         : trie_(k),
           k_(k),
-          threshold_(0),
-          max_freq_(0) {
+          max_allowed_frequency_(sketch_columns-1),
+          threshold_(0) {
         
+        if(sketch_columns == 0) {
+            std::cerr << "need at least one column" << std::endl;
+            std::abort();
+        }
+
         // initialize all k nodes as orphans in trie
         trie_.fill();
 
@@ -186,8 +225,11 @@ public:
         }
 
         // create initial garbage bucket that contains all nodes
-        bucket_ends_.reserve(k);
-        bucket_ends_[0] = num_slots() - 1;
+        bucket_ends_ = std::make_unique<TrieNodeIndex[]>(max_allowed_frequency_ + 1);
+        bucket_ends_[0] = num_freqs() - 1;
+        for(size_t i = 1; i <= max_allowed_frequency_; i++) {
+            bucket_ends_[i] = BUCKET_DOES_NOT_EXIST;
+        }
     }
 
     struct StringState {
@@ -235,10 +277,6 @@ public:
         }
 
         // advance
-        if constexpr(DEBUG) {
-            std::cout << "top-k: extend string of length " << s.len << " (node " << s.node << ") by " << display(c) << " -> node=" << ext.node << ", fp=" << ext.fingerprint << ", frequent=" << ext.frequent << std::endl;
-        }
-
         return ext;
     }
 
@@ -268,18 +306,17 @@ public:
     void print_debug_info() const {
         trie_.print_debug_info();
 
-        std::cout << "# DEBUG: misra-gries2"
-            << ", threshold=" << threshold_
-            << ", buckets=" << bucket_ends_.size()
-            << ", max_freq=" << max_freq_
-            ;
-        
+        std::cout << "# DEBUG: misra-gries2" << ", threshold=" << threshold_;
         if constexpr(gather_stats_) {
             std::cout
+                << ", max_freq=" << stats_.max_freq
                 << ", num_increment=" << stats_.num_increment
                 << ", num_swap=" << stats_.num_swap
                 << ", num_decrement=" << stats_.num_decrement
+                << ", num_renormalize=" << stats_.num_renormalize
                 ;
+        } else {
+            std::cout << " (advanced stats disabled)";
         }
 
         std::cout << std::endl;
