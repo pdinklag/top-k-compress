4e0311df5b2aced6ec013bcc05e870c1dffdd48a
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2024-01-24 12:25:40 +0100

    Control LZ78 memory peak

--- include/simple_trie.hpp
+++ include/simple_trie.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <memory>
 #include <vector>
+
 #include "trie_edge_array.hpp"
 
 class SimpleTrie {
@@ -9,13 +11,32 @@ public:
     using NodeData = TrieEdgeArray<char, Node>;
 
 private:
-    std::vector<NodeData> nodes_;
+    static constexpr size_t block_size_ = 1ULL << 20;
+    static constexpr size_t block_mask_ = block_size_ - 1;
+
+    std::vector<std::unique_ptr<NodeData[]>> blocks_;
+    size_t size_;
+    size_t capacity_;
+
+    NodeData& node(Node const i) {
+        auto const block = i / block_size_;
+        return blocks_[block][i & block_mask_];
+    }
 
     Node insert_child(Node const parent, char const c) {
-        auto const v = size();
-        nodes_.push_back(NodeData());
-        nodes_[parent].insert(c, v);
+        auto const v = size_++;
+        if(v == capacity_) {
+            blocks_.emplace_back(alloc_block());
+            capacity_ += block_size_;
+        }
+
+        node(v) = NodeData();
+        node(parent).insert(c, v);
         return v;
+    }
+
+    std::unique_ptr<NodeData[]> alloc_block() {
+        return std::make_unique<NodeData[]>(block_size_);
     }
 
 public:
@@ -25,10 +46,10 @@ public:
 
     Node root() const { return 0; }
 
-    size_t size() const { return nodes_.size(); }
+    size_t size() const { return size_; }
 
     bool follow_edge(Node const v, char const c, Node& out_node) {
-        auto const found = nodes_[v].try_get(c, out_node);
+        auto const found = node(v).try_get(c, out_node);
         if(!found) {
             out_node = insert_child(v, c);
         }
@@ -36,7 +57,10 @@ public:
     }
 
     void clear() {
-        nodes_.clear();
-        nodes_.push_back(NodeData());
+        size_ = 1;
+        capacity_ = block_size_;
+        blocks_.clear();
+        blocks_.emplace_back(alloc_block());
+        node(0) = NodeData();
     }
 };
