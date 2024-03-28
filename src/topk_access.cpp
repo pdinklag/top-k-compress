ec42d4fb998f6e7ed75b5a0c11743cf904976da0
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2024-03-28 07:38:03 +0100

    Implement access for topk-access data structure

--- src/topk_access.cpp
+++ src/topk_access.cpp
@@ -20,6 +20,8 @@ int main(int argc, char** argv) {
     Application app(options, argc, argv);
     if(app) {
         if(app.args().size() == 1) {
+            // construct
+            std::cout << "constructing ..." << std::endl;
             pm::Stopwatch sw;
             pm::MallocCounter mem;
 
@@ -29,11 +31,38 @@ int main(int argc, char** argv) {
             sw.stop();
             mem.stop();
 
+            auto const t_construct = (uintmax_t)sw.elapsed_time_millis();
+            auto const mem_peak = mem.peak();
+            auto const mem_text = mem.count();
+
+            // decode one by one
+            std::cout << "decoding one by one ..." << std::endl;
+            std::string dec;
+            sw.start();
+            {
+                size_t n = text.length();
+                
+                dec.reserve(n);
+
+                for(size_t i = 0; i < n; i++) {
+                    dec.push_back(text[i]);
+                }
+            }
+            sw.stop();
+            auto const t_decode_1by1 = (uintmax_t)sw.elapsed_time_millis();
+
+            {
+                iopp::FileOutputStream fos(app.args()[0] + ".dec");
+                fos.write(dec.data(), dec.length());
+            }
+
             pm::Result r;
-            r.add("num_phrases", text.num_phrases());
-            r.add("time_construct", (uintmax_t)sw.elapsed_time_millis());
-            r.add("mem_peak", mem.peak());
-            r.add("mem_text", mem.count());
+            r.add("n", text.length());
+            r.add("z", text.num_phrases());
+            r.add("time_construct", t_construct);
+            r.add("time_decode_1by1", t_decode_1by1);
+            r.add("mem_peak", mem_peak);
+            r.add("mem_text", mem_text);
 
             r.print();
             return 0;
--- src/topk_access.hpp
+++ src/topk_access.hpp
@@ -8,12 +8,14 @@
 #include <ordered/range_marking/set.hpp>
 #include <word_packing.hpp>
 
+#include <bv/bit_rank.hpp>
 #include <idiv_ceil.hpp>
 #include <small_trie.hpp>
 
 class TopKAccess {
 private:
     using Pack = uintmax_t;
+    using Rank = BitRank<Pack>;
 
     size_t n_;
     size_t k_;
@@ -21,8 +23,33 @@ private:
     size_t height_;
     word_packing::PackedIntVector<Pack> parsing_;
     word_packing::BitVector literal_;
-    std::unique_ptr<char[]> dec_buffer_;
     std::unique_ptr<Pack[]> start_;
+    Rank start_rank_;
+
+    std::unique_ptr<char[]> dec_buffer_;
+    mutable size_t dec_last_; // nb: cache to avoid re-spelling of the same phrase on subsequent queries -- modified in a const function
+    mutable size_t dec_last_len_;
+
+    struct PhraseContaining {
+        size_t phrase;
+        size_t start;
+    };
+
+    PhraseContaining phrase_containing(size_t const i) const {
+        // query what phrase contains position i of the original input
+        PhraseContaining r;
+        r.phrase = start_rank_(i) - 1;
+        assert(r.phrase != SIZE_MAX);
+
+        // find the starting position of that phrase
+        // nb: naive select query, but since start_ is expected to be dense, we shouldn't have to scan for long
+        auto start = word_packing::bit_accessor(start_.get());
+        size_t j = i;
+        while(!start[j]) --j;
+        r.start = j;
+
+        return r;
+    }
 
 public:
     TopKAccess(std::filesystem::path const& path, size_t const k)
@@ -64,19 +91,37 @@ public:
             start[i] = 1;
             i += f.len;
         });
+        assert(i == n_);
 
         parsing_.shrink_to_fit();
         literal_.shrink_to_fit();
 
-        // TODO: compute rank/select data structure on start
-
+        // compute rank/select data structure on start
+        start_rank_ = Rank(start_.get(), n_);
+        assert(start_rank_(n_ - 1) == parsing_.size());
+        
         // initialize decode buffer
         dec_buffer_ = std::make_unique<char[]>(height_);
+        dec_last_ = -1;
     }
 
     char operator[](size_t const i) const {
-        // TODO: implement single-character access
-        return 0;
+        auto const p = phrase_containing(i);
+        
+        auto const v = parsing_[p.phrase];
+        if(literal_[p.phrase]) {
+            // literal phrase, extract the character
+            return (char)v;
+        } else {
+            // trie phrase - spell out and extract character
+            if(dec_last_ != v) {
+                dec_last_len_ = trie_.spell_reverse(v, dec_buffer_.get());
+                dec_last_ = v;
+            }
+        
+            auto const offs = (dec_last_len_ - 1) - (i - p.start);
+            return dec_buffer_[offs];
+        }
     }
 
     void extract(size_t const i, size_t const len, char* buffer) const {
@@ -85,17 +130,14 @@ public:
 
     size_t lce(size_t const i, size_t const j) const {
         // TODO: implement LCE query
+        return 0;
     }
 
     size_t num_phrases() const {
         return parsing_.size();
     }
 
-    size_t mem_size() const {
-        return sizeof(TopKAccess) +
-            idiv_ceil(parsing_.capacity() * parsing_.width(), 64) * sizeof(Pack) +
-            idiv_ceil(literal_.capacity(), 64) * sizeof(Pack) +
-            idiv_ceil(n_, 8);
-            height_;
+    size_t length() const {
+        return n_;
     }
 };
